df3280bd421bdc1ad5fc8d218168341cf0cc916b
Woonsan Ko <woonsan.ko@bloomreach.com>
2014-11-03 19:09:36 +0000

    migrating wicket 1.5 dependent code to 6.0 compliant code

--- src/main/java/org/onehippo/forge/iframeprspectiv/GenericIFramePerspective.java
+++ src/main/java/org/onehippo/forge/iframeprspectiv/GenericIFramePerspective.java
@@ -15,13 +15,16 @@
  */
 package org.onehippo.forge.iframeprspectiv;
 
-import org.apache.wicket.ResourceReference;
-import org.apache.wicket.behavior.AbstractBehavior;
-import org.apache.wicket.behavior.SimpleAttributeModifier;
-import org.apache.wicket.markup.html.CSSPackageResource;
-import org.apache.wicket.markup.html.IHeaderResponse;
-import org.apache.wicket.markup.html.JavascriptPackageResource;
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.request.resource.CssResourceReference;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+import org.apache.wicket.request.resource.PackageResourceReference;
+import org.apache.wicket.request.resource.ResourceReference;
 import org.hippoecm.frontend.dialog.IDialogService;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
@@ -29,62 +32,59 @@ import org.hippoecm.frontend.plugins.standards.perspective.Perspective;
 import org.hippoecm.frontend.service.IconSize;
 
 public class GenericIFramePerspective extends Perspective {
-    
+
     private static final String DEFAULT_ICON_PREFIX = "generic-iframe-perspective-";
     private static final String DEFAULT_ICON_SUFFIX = ".png";
 
+    private static final ResourceReference GENERIC_IFRAME_CSS = new CssResourceReference(GenericIFramePerspective.class, "generic-iframe-perspective.css");
+    private static final ResourceReference GENERIC_IFRAME_JS = new JavaScriptResourceReference(GenericIFramePerspective.class, "generic-iframe-perspective.js");
+
     private static final long serialVersionUID = 1L;
-    
+
     private static final String IFRAME_ATTRIBUTE_PREFIX = "iframe.";
-    
+
     private String iconPrefix;
     private String iconSuffix;
+
+    private final WebMarkupContainer iframe;
 
     public GenericIFramePerspective(IPluginContext context, IPluginConfig config) {
         super(context, config);
         setOutputMarkupId(true);
 
-        add(CSSPackageResource.getHeaderContribution(getClass(), "generic-iframe-perspective.css"));
-        add(JavascriptPackageResource.getHeaderContribution(getClass(), "generic-iframe-perspective.js"));
-        
-        final WebMarkupContainer iframe = new WebMarkupContainer("generic-perspective-iframe");
+        iframe = new WebMarkupContainer("generic-perspective-iframe");
         iframe.setOutputMarkupId(true);
-        
+
         iconPrefix = config.getString("icon-prefix", DEFAULT_ICON_PREFIX);
         iconSuffix = config.getString("icon-suffix", DEFAULT_ICON_SUFFIX);
-        
+
         for (String key : config.keySet()) {
             if (key.startsWith(IFRAME_ATTRIBUTE_PREFIX)) {
                 String attrName = key.substring(IFRAME_ATTRIBUTE_PREFIX.length());
                 String attrValue = config.getString(key, null);
-                
+
                 if (attrValue != null) {
-                    iframe.add(new SimpleAttributeModifier(attrName, attrValue));
+                    iframe.add(new AttributeModifier(attrName, attrValue));
                 }
             }
         }
-        
-        add(iframe);
-        
-        add(new AbstractBehavior() {
-            private static final long serialVersionUID = 1L;
 
-            @Override
-            public void renderHead(IHeaderResponse response) {
-                String js = "GenericIFramePerspective.showIFrame(\"" + iframe.getMarkupId() + "\");";
-                response.renderOnDomReadyJavascript(js);
-                super.renderHead(response); 
-            } 
-        });
+        add(iframe);
+    }
+
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        response.render(CssHeaderItem.forReference(GENERIC_IFRAME_CSS));
+        response.render(JavaScriptHeaderItem.forReference(GENERIC_IFRAME_JS));
+        response.render(OnDomReadyHeaderItem.forScript("GenericIFramePerspective.showIFrame(\"" + iframe.getMarkupId() + "\");"));
     }
 
     @Override
     public ResourceReference getIcon(IconSize type) {
-        return new ResourceReference(GenericIFramePerspective.class, iconPrefix + type.getSize() + iconSuffix);
+        return new PackageResourceReference(GenericIFramePerspective.class, iconPrefix + type.getSize() + iconSuffix);
     }
 
     public void showDialog(IDialogService.Dialog dialog) {
         getPluginContext().getService(IDialogService.class.getName(), IDialogService.class).show(dialog);
     }
-
 }
