f404796740fece4b56c1fc1cb95c76cfffd6eca8
Woonsan Ko <woonsan.ko@bloomreach.com>
2020-04-30 17:07:49 -0400

    HIPFORGE-346: upgrade iframe perspective plugin for v14

--- src/main/java/org/onehippo/forge/iframeprspectiv/GenericIFramePerspective.java
+++ src/main/java/org/onehippo/forge/iframeprspectiv/GenericIFramePerspective.java
@@ -22,42 +22,28 @@ import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.request.Response;
 import org.apache.wicket.request.cycle.RequestCycle;
 import org.apache.wicket.request.http.WebResponse;
 import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
-import org.apache.wicket.request.resource.PackageResourceReference;
 import org.apache.wicket.request.resource.ResourceReference;
 import org.hippoecm.frontend.dialog.IDialogService;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
 import org.hippoecm.frontend.plugins.standards.perspective.Perspective;
-import org.hippoecm.frontend.service.IconSize;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@SuppressWarnings("serial")
 public class GenericIFramePerspective extends Perspective {
 
     private static Logger log = LoggerFactory.getLogger(GenericIFramePerspective.class);
 
-    private static final String TITLE = "perspective.title";
-
-    private static final String DEFAULT_ICON_PREFIX = "generic-iframe-perspective-";
-    private static final String DEFAULT_ICON_SUFFIX = ".png";
-
     private static final ResourceReference GENERIC_IFRAME_CSS = new CssResourceReference(GenericIFramePerspective.class, "generic-iframe-perspective.css");
     private static final ResourceReference GENERIC_IFRAME_JS = new JavaScriptResourceReference(GenericIFramePerspective.class, "generic-iframe-perspective.js");
 
-    private static final long serialVersionUID = 1L;
-
     private static final String IFRAME_ATTRIBUTE_PREFIX = "iframe.";
-
-    private IModel<String> titleModel;
-    private String iconPrefix;
-    private String iconSuffix;
 
     /**
      * X-Frame-Options.
@@ -92,14 +78,8 @@ public class GenericIFramePerspective extends Perspective {
         super(context, config);
         setOutputMarkupId(true);
 
-        final String titleKey = config.getString(TITLE, TITLE_KEY);
-        titleModel = new StringResourceModel(titleKey, this);
-
         iframe = new WebMarkupContainer("generic-perspective-iframe");
         iframe.setOutputMarkupId(true);
-
-        iconPrefix = StringUtils.trim(config.getString("icon-prefix", DEFAULT_ICON_PREFIX));
-        iconSuffix = StringUtils.trim(config.getString("icon-suffix", DEFAULT_ICON_SUFFIX));
 
         xFrameOptions = StringUtils.trim(config.getString("x-frame-options", null));
         contentSecurityPolicy = StringUtils.trim(config.getString("content-security-policy", null));
@@ -118,11 +98,6 @@ public class GenericIFramePerspective extends Perspective {
         }
 
         add(iframe);
-    }
-
-    @Override
-    public IModel<String> getTitle() {
-        return titleModel;
     }
 
     @Override
@@ -157,11 +132,6 @@ public class GenericIFramePerspective extends Perspective {
         response.render(CssHeaderItem.forReference(GENERIC_IFRAME_CSS));
         response.render(JavaScriptHeaderItem.forReference(GENERIC_IFRAME_JS));
         response.render(OnDomReadyHeaderItem.forScript("GenericIFramePerspective.showIFrame(\"" + iframe.getMarkupId() + "\");"));
-    }
-
-    @Override
-    public ResourceReference getIcon(IconSize type) {
-        return new PackageResourceReference(GenericIFramePerspective.class, iconPrefix + type.getSize() + iconSuffix);
     }
 
     public void showDialog(IDialogService.Dialog dialog) {
---
+++ src/main/java/org/onehippo/forge/iframeprspectiv/GenericIFramePerspective4.java
@@ -0,0 +1,32 @@
+/*
+ *  Copyright 2020 Bloomreach, Inc.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.onehippo.forge.iframeprspectiv;
+
+import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.config.IPluginConfig;
+
+/**
+ * Convenient class providing the same functionality as {@link GenericIFramePerspective} when configuring the
+ * fourth {@link GenericIFramePerspective} based extension perspective only through configuration.
+ */
+@SuppressWarnings("serial")
+public class GenericIFramePerspective4 extends GenericIFramePerspective {
+
+    public GenericIFramePerspective4(IPluginContext context, IPluginConfig config) {
+        super(context, config);
+    }
+
+}
